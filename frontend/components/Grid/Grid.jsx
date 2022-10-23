6848da2c11494fabee425b06d341a695cabd1eb5
nathallyet <nathallyet@gmail.com>
2022-10-23 00:12:03 -0300

    created components Grid and Button

--- frontend/components/Grid/Grid.jsx
+++ frontend/components/Grid/Grid.jsx
@@ -6,10 +6,10 @@ class Grid extends Component {
     const cols = numbers ? numbers.split(" ") : []; // numbers existe ? se verdadeiro aplicar o método split no array numbers, senão : retornar um array vazio 
     let classes = "";
 
-    if(cols[0]) classes+= `col-xs-${cols[0]}`;
-    if(cols[1]) classes+= `col-sm-${cols[1]}`;
-    if(cols[2]) classes+= `col-md-${cols[2]}`;
-    if(cols[3]) classes+= `col-lg-${cols[3]}`;
+    if(cols[0]) classes += `col-xs-${cols[0]}`;
+    if(cols[1]) classes += ` col-sm-${cols[1]}`;
+    if(cols[2]) classes += ` col-md-${cols[2]}`;
+    if(cols[3]) classes += ` col-lg-${cols[3]}`;
 
     return classes;
   }
@@ -17,8 +17,12 @@ class Grid extends Component {
   render() {
     const gridClasses = this.toCssClasses(this.props.cols || 12); // se cols não foi setado cols será 12
 
+    console.log(gridClasses);
+    console.log(this.props.cols);
+    console.log(this.props);
+
     return (
-      <div>
+      <div className={gridClasses}>
         {this.props.children}
       </div>
     )
